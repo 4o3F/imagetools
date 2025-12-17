cd07b1d3d92e435324ffaa3faa5d15b00c7446b1
403F <4o3f@proton.me>
2025-12-17 12:31:40 +0800

    chore: try to use static opencv on windows

--- scripts/install-windows-vcpkg.sh
+++ scripts/install-windows-vcpkg.sh
@@ -5,7 +5,7 @@ set -xeu
 # install llvm from choco in place of vcpkg to speed things up
 choco install -y llvm --version "$CHOCO_LLVM_VERSION"
 
-export VCPKG_DEFAULT_TRIPLET=x64-windows
+export VCPKG_DEFAULT_TRIPLET=x64-windows-static
 
 script_dir="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
